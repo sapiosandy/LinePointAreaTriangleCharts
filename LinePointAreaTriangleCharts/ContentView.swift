83ceafa2c85b71c7e677b3f0c6d7c57b9f97749a
Sandy Gomez <sgomez1029@gmail.com>
2024-04-10 13:08:15 -0700

    LinePointAreaTriangleCharts

--- LinePointAreaTriangleCharts/ContentView.swift
+++ LinePointAreaTriangleCharts/ContentView.swift
@@ -6,16 +6,65 @@
 //
 
 import SwiftUI
+import Charts
+
+struct SalesPeople: Identifiable {
+    var name: String
+    var sales: Int
+    var id: String {name}
+}
+
+var mySalesArray: [SalesPeople] = [
+    .init(name: "Oscar", sales: 50),
+    .init(name: "Mayer", sales: 43),
+    .init(name: "George", sales: 62),
+    .init(name: "Hormel", sales: 26)
+]
+
+enum ChartType {
+    case bar
+    case line
+    case point
+    case area
+    case rectangle
+}
 
 struct ContentView: View {
+    @State var whichChart: ChartType = ChartType.bar
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Chart(mySalesArray) { z in
+                switch whichChart {
+                case .bar:
+                    BarMark(x: .value("Name", z.name),
+                            y: .value("Sales", z.sales))
+                    
+                case .line:
+                    LineMark(x: .value("Name", z.name),
+                             y: .value("Sales", z.sales))
+                    
+                case .point:
+                    PointMark(x: .value("Name", z.name),
+                              y: .value("Sales", z.sales))
+                    
+                case .area:
+                    AreaMark(x: .value("Name", z.name),
+                             y: .value("Sales", z.sales))
+                    
+                case .rectangle:
+                    RectangleMark(x: .value("Name", z.name),
+                                  y: .value("Sales", z.sales))
+                }
+            }
+            Picker(selection: $whichChart, label: Text("")) {
+                Text("Bar chart").tag(ChartType.bar)
+                Text("Line chart").tag(ChartType.line)
+                Text("Point chart").tag(ChartType.point)
+                Text("Area chart").tag(ChartType.area)
+                Text("Rectangle chart").tag(ChartType.rectangle)
+            }
         }
-        .padding()
     }
 }
 

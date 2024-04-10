bed46c7720fc81bd35cf26c1fbf9342534daada5
Sandy Gomez <sgomez1029@gmail.com>
2024-04-10 12:26:04 -0700

    Initial Commit

---
+++ LinePointAreaTriangleCharts/LinePointAreaTriangleChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinePointAreaTriangleChartsApp.swift
+//  LinePointAreaTriangleCharts
+//
+//  Created by Sandra Gomez on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LinePointAreaTriangleChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
